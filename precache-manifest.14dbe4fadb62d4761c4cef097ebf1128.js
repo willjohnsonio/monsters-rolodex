90edf9512c9efc97231b49afe7387bc17589ee57
William Johnson <w.alexander.johnson@gmail.com>
2019-07-29 05:22:44 -0500

    Updates

---
+++ precache-manifest.14dbe4fadb62d4761c4cef097ebf1128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2b04baf85f85206c3458317a8af91d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ed9387e8264306dd3f81",
+    "url": "/monsters-rolodex/static/css/main.1daf98a7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ed9387e8264306dd3f81",
+    "url": "/monsters-rolodex/static/js/main.fa316202.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
